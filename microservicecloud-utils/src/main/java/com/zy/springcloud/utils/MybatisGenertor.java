df73130c52bd5600f612e65caab76314e9a7306e
ZhouYuanZZZZ <1270157082@qq.com>
2018-12-24 18:25:16 +0800

    init

---
+++ microservicecloud-utils/src/main/java/com/zy/springcloud/utils/MybatisGenertor.java
@@ -0,0 +1,32 @@
+package com.zy.springcloud.utils;
+
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.exception.InvalidConfigurationException;
+import org.mybatis.generator.exception.XMLParserException;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MybatisGenertor {
+
+    public static void main(String[] args) throws IOException, XMLParserException, InvalidConfigurationException, SQLException, InterruptedException {
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config =
+                cp.parseConfiguration(MybatisGenertor.class.getClassLoader().getResourceAsStream("generator.xml"));
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        myBatisGenerator.generate(null);
+        for (String warning : warnings) {
+            System.out.println(warning);
+        }
+    }
+
+}
