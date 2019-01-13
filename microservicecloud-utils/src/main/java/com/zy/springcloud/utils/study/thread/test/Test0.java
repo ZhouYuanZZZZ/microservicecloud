accb1da242eb16fa73670a1210e8b5b398d3420f
zy <zy1270157082@outlook.com>
2019-01-14 01:40:35 +0800

    init

---
+++ microservicecloud-utils/src/main/java/com/zy/springcloud/utils/study/thread/test/Test0.java
@@ -0,0 +1,13 @@
+package com.zy.springcloud.utils.study.thread.test;
+
+import org.junit.Test;
+
+public class Test0 {
+
+    @Test
+    public void test0(){
+        short s1 = 1;
+        s1+=1;
+        System.out.println(s1);
+    }
+}
