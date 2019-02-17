20e8fb47e58ac9b7cdacbdb729a493d0cf274aac
ZhouYuanZZZZ <zy1270157082@outlook.com>
2019-02-17 16:22:44 +0800

    ...

--- microservicecloud-utils/src/main/java/com/zy/springcloud/utils/study/thread/test/Test0.java
+++ microservicecloud-utils/src/main/java/com/zy/springcloud/utils/study/thread/test/Test0.java
@@ -10,4 +10,16 @@ public class Test0 {
         s1+=1;
         System.out.println(s1);
     }
+
+    @Test
+    public void test1(){
+
+        System.out.println(Math.floor(3.1));
+        System.out.println(Math.floor(3.12));
+        System.out.println(Math.floor(3.147));
+        System.out.println(Math.floor(3.1785));
+        System.out.println(Math.floor(-3.7575757));
+
+        System.out.println(Math.round(3.648));
+    }
 }
